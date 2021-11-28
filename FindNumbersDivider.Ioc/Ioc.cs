588fba4daf9ac666bbef4f75abd8b2afe540d617
Filipi Carriço Mosquini <filipimosquini@gmail.com>
2021-11-28 19:12:24 -0300

    Configura a injeção de dependências

---
+++ FindNumbersDivider.Ioc/Ioc.cs
@@ -0,0 +1,18 @@
+﻿using FindNumbersDivider.Application;
+using FindNumbersDivider.Application.Interfaces;
+using FindNumbersDivider.Domain.Services;
+using FindNumbersDivider.Domain.Services.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FindNumbersDivider.Ioc
+{
+    public static class Ioc
+    {
+        public static IServiceCollection SolveDependencies(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<INumberService, NumberService>()
+                .AddScoped<IFindNumbersDividerAppService, FindNumbersDividerAppService>();
+        }
+    }
+}
