c462d2125575e3371c2c8a8647688eccc20f3279
Filipi Carriço Mosquini <filipimosquini@gmail.com>
2021-11-28 09:41:22 -0300

    Commit inicial

---
+++ FindNumersDivider.Entrypoint/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DivisoresDeUmNumero
+{
+    /// <summary>
+    /// Calcula os divisores de um número
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
